65388ac49f1fc293a207f5dae42a3496141510a9
Shehroz <shehrozehsan@gmail.com>
2020-10-21 19:30:58 +0500

    Class & Module Mixin

--- student.rb
+++ student.rb
@@ -2,24 +2,24 @@ require_relative 'user_module'
 
 class Student
 
-    include Crud
-
-    attr_accessor :first_name, :last_name, :email, :username, :password
-
-    def initialize(firstname, lastname, email, username, password)
-        @first_name = firstname
-        @last_name = lastname
-        @email = email
-        @username = username
-        @password = password
-    end
-
-    def to_s
-        "Fisrt name: #{@first_name}, Last name: #{@last_name}, Email: #{@email}, Username: #{@username}"
-    end
+  include Crud
+  
+  attr_accessor :first_name, :last_name, :email, :username, :password
+  
+  def initialize(firstname, lastname, email, username, password)
+    @first_name = firstname
+    @last_name = lastname
+    @email = email
+    @username = username
+    @password = password
+  end
+  
+  def to_s
+      "Fisrt name: #{@first_name}, Last name: #{@last_name}, Email: #{@email}, Username: #{@username}"
+  end
 
 end
-
+    
 
 Shehroz = Student.new("Muhammad","Shehroz","shehroz@gmail.com","shehroz94","asd")
 
